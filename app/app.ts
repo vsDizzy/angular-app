e740ca7b09fdee91f002f2a97588bd0910d81de4
Dizzy <todizzy@gmail.com>
2016-03-03 08:13:35 +0200

    ---

--- app/app.ts
+++ app/app.ts
@@ -1,10 +1,41 @@
-import {Component} from 'angular2/core';
-
-import {bootstrap} from 'angular2/platform/browser';
-bootstrap(App);
+import {Component, EventEmitter} from 'angular2/core';
 
 @Component({
-  selector: 'app', template: 'hello worl4d'
+  selector: 'child',
+  template: `
+    <div>
+      counter: {{counter}}
+    </div>
+  `,
+  inputs: ['counter'],
+  outputs: ['changed']
+})
+export class Child {
+  public counter: number;
+  public changed: EventEmitter<any> = new EventEmitter();
+
+  constructor() {
+    setInterval(() => {
+      this.counter++;
+      this.changed.emit(this);
+    }, 1000);
+  }
+}
+
+@Component({
+  selector: 'app',
+  directives: [Child],
+  template: `
+    <div>
+      current: {{current}}
+      <child [counter]="current" (changed)="cc($event)"></child>
+    </div>
+  `
 })
 export class App {
+  current: number = 3;
+
+  cc(evt) {
+    this.current = evt.counter;
+  }
 }
