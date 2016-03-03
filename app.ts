836de683f9333892c9c257f161f88bf6efc802eb
Dizzy <todizzy@gmail.com>
2016-03-03 03:56:40 +0200

    `

--- app.ts
+++
@@ -1,5 +0,0 @@
-import {Component} from 'angular2/core';
-
-@Component({ selector: 'my-app', template: 'hello world' })
-export class App {
-}
---
+++ app/app.ts
@@ -0,0 +1,10 @@
+import {Component} from 'angular2/core';
+
+import {bootstrap} from 'angular2/platform/browser';
+bootstrap(App);
+
+@Component({
+  selector: 'app', template: 'hello worl4d'
+})
+export class App {
+}
