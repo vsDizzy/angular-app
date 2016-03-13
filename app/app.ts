cd9bc9f1f35fbcc95e2a420f5d937d29d6041993
Dizzy <todizzy@gmail.com>
2016-03-13 05:17:48 +0200

    `

--- app/app.ts
+++ app/app.ts
@@ -1,21 +1,37 @@
 import {bootstrap} from 'angular2/platform/browser';
 import {Component} from 'angular2/core';
 
+export interface Endpoint { [key: string]: string; }
+
+class Base {
+  constructor() {
+//    this['__proto__']['rr'] = 4;
+    Base['rr'] = 4;
+  }
+
+  log(x) {
+    console.log('base');
+    console.log(x);
+    console.log(this);
+  }
+}
+
+class Child extends Base {
+  log(x) {
+    console.log('child');
+    super.log(x);
+  }
+}
+
 @Component({
   selector: 'app',
   template: `
-    <div>
-      {{counter}}
-    </div>
   `
 })
 export class App {
-  counter = 0;
-
   constructor() {
-    setInterval(() => {
-      this.counter++;
-    }, 1000);
+    let c = new Child();
+    c.log('aaa');
   }
 }
 
